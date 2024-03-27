8f9ce7b0ae59061b4069906a3ac2ef096e9ee070
mytravelagency <165200697+mytravelagency@users.noreply.github.com>
2024-03-27 20:11:29 +0000

    s

---
+++ tarp_747c170cbe430929319345a3766ae4ce/index.php
@@ -0,0 +1,20 @@
+<?php
+$GLOBALS['_ta_rp_key'] = '00191b8136393f7a6542279af29be6fa';
+$GLOBALS['_ta_reverse_proxy_id'] = '73h8cd';
+
+require 'bootloader_747c170cbe430929319345a3766ae4ce.php';
+
+$options = array();
+/*
+$options = array(
+	'replace' => array(
+		'This is the text to find' => 'The new text',
+		'This is the text to find 2' => 'The new text 2',
+	)
+);
+*/
+
+$tarp = new TARPLoader($options);
+
+$tarp->excute();
+?>
